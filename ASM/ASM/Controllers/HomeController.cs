a066fce7c21cee998dffc564d087cf8e6e9ed608
minhhoang0xx <118556260+minhhoang0xx@users.noreply.github.com>
2023-10-22 18:00:52 +0700

    Add funtion to HomeController

--- ASM/ASM/Controllers/HomeController.cs
+++ ASM/ASM/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using ASM.Models;
+﻿using ASM.Areas.Identity.Data;
+using ASM.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,16 +9,51 @@ namespace ASM.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ASMDBContext _db;
+        private readonly UserManager<ManageUser> _userManager;
+        public HomeController(ILogger<HomeController> logger, ASMDBContext context, UserManager<ManageUser> userManager)
         {
             _logger = logger;
+            _db = context;
+            _userManager = userManager;
         }
 
+        //hienthibook
         public IActionResult Index()
         {
+            var category = getAllCategory();
+            ViewBag.category = category;
             return View();
         }
+        public IActionResult ListBook()
+        {
+            var _product = getAllBook();
+            ViewBag.book = _product;
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+            var book = _db.Book.Find(id);
+            return View(book);
+        }
+        //hamphu
+        public List<Book> getAllBook()
+        {
+            return _db.Book.ToList();
+        }
+        private List<Category> getAllCategory()
+        {
+            return _db.Category.ToList();
+        }
+        private Book getDetailBook(int id)
+        {
+            return _db.Book.Find(id);
+        }
+        private Cart getCart(int id)
+        {
+            return _db.Cart.Find(id);
+        }
+        //
 
         public IActionResult Privacy()
         {
