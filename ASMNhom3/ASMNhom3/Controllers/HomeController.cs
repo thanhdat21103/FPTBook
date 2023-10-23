e032c610333673e46359dad87fcb71df6f4f2a3f
tutruong193 <128039200+tutruong193@users.noreply.github.com>
2023-10-24 01:30:36 +0700

    add checkout in controller and confirm checkout
    add checkout in controller and confirm checkout

--- ASMNhom3/ASMNhom3/Controllers/HomeController.cs
+++ ASMNhom3/ASMNhom3/Controllers/HomeController.cs
@@ -106,6 +106,75 @@ namespace ASMNhom3.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+        public IActionResult ChecKOut()
+        {
+            if (HttpContext.Session.GetString("Email") != null)
+            {
+                var userEmail = HttpContext.Session.GetString("Email");
+                List<Cart> carts = new List<Cart>();
+                ViewBag.TotalPrice = 0;
+                ViewBag.TotalQuanlity = 0;
+                foreach (var cart in _db.Carts)
+                {
+                    if (userEmail == cart.EmailUser)
+                    {
+                        carts.Add(cart);
+                    }
+                }
+                foreach (var cart in _db.Carts)
+                {
+                    foreach (var book in _db.Books)
+                    {
+                        if (userEmail == cart.EmailUser && cart.BookID == book.BookID)
+                        {
+                            ViewBag.TotalPrice += cart.Total;
+                            ViewBag.TotalQuanlity += cart.Quanlity;
+                        }
+                    }
+                }
+                ViewBag.book = getAllBook();
+                ViewBag.product = carts;
+                ViewBag.user = _db.Accounts.FirstOrDefault(c => c.Email == userEmail);
+                return View();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult ConfirmCheckOut(QueueCheckOut model)
+        {
+            var userEmail = HttpContext.Session.GetString("Email");
+            if (ModelState.IsValid)
+            {
+                var newQueue = new QueueCheckOut
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    PhoneNum = model.PhoneNum,
+                    Address = model.Address,
+                    Email = model.Email,
+                    Note = model.Note,
+                    TotalPrice = model.TotalPrice,
+                    TotalQuantity = model.TotalQuantity,
+                    IsConfirm = false
+                };
+                _db.QueueCheckOuts.Add(newQueue);
+                List<Cart> oldcart = new List<Cart>();
+                foreach (var cart in _db.Carts)
+                {
+                    if (cart.EmailUser == userEmail)
+                    {
+                        oldcart.Add(cart);
+                    }
+                }
+                for (int i = 0; i < oldcart.Count(); i++)
+                {
+                    _db.Carts.Remove(oldcart[i]);
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("ChecKOut");
+        }
         //ko can dang nhap, hien thi book
         public IActionResult Index()
         {
