f488c75b1872b59342eecfd35090a450d5a3d36b
tutruong193 <128039200+tutruong193@users.noreply.github.com>
2023-10-24 01:18:17 +0700

    add addcart, cartlist, deletecart
    add addcart, cartlist, deletecart

--- ASMNhom3/ASMNhom3/Controllers/HomeController.cs
+++ ASMNhom3/ASMNhom3/Controllers/HomeController.cs
@@ -13,6 +13,99 @@ namespace ASMNhom3.Controllers
             _logger = logger;
             _db = context;
         }
+        //cart
+        public IActionResult addCart(int id, int quanlity)
+        {
+            if (HttpContext.Session.GetString("Email") != null)
+            {
+                var userEmail = HttpContext.Session.GetString("Email");
+                var product = getDetailBook(id);
+                Cart existingCartItem = null;
+                foreach (var cart in _db.Carts)
+                {
+                    if (cart.BookID == product.BookID && userEmail == cart.EmailUser)
+                    {
+                        existingCartItem = cart;
+                        break;
+                    }
+                }
+
+                if (existingCartItem != null)
+                {
+                    existingCartItem.Quanlity += quanlity;
+                }
+                else
+                {
+                    var newCartItem = new Cart
+                    {
+                        EmailUser = userEmail,
+                        BookID = product.BookID,
+                        Quanlity = quanlity,
+                        Price = product.Price,
+                        Total = quanlity * product.Price
+                    };
+                    _db.Carts.Add(newCartItem);
+                }
+                product.Quantity -= quanlity;
+                _db.SaveChanges(); // Lưu dữ liệu vào cơ sở dữ liệu
+
+                return RedirectToAction("BookDetail", "Home", new { id = product.BookID });
+            }
+            else
+            {
+                return RedirectToAction("Login", "Login");
+            }
+        }
+        public IActionResult deleteCart(int id)
+        {
+            if (HttpContext.Session.GetString("Email") != null)
+            {
+                ViewBag.UserEmail = HttpContext.Session.GetString("Email");
+                var cart = getCart(id);
+                if (cart != null)
+                {
+                    var product = getDetailBook(cart.BookID);
+                    if (product != null)
+                    {
+                        product.Quantity += cart.Quanlity;
+                        _db.Carts.Remove(cart);
+                        _db.SaveChanges();
+                    }
+                }
+            }
+            return RedirectToAction("CartList", "Home");
+        }
+
+        public IActionResult CartList()
+        {
+            if (HttpContext.Session.GetString("Email") != null)
+            {
+                ViewBag.UserEmail = HttpContext.Session.GetString("Email");
+                var userEmail = HttpContext.Session.GetString("Email");
+                var userCarts = _db.Carts
+                    .Where(c => c.EmailUser == userEmail)// Include thông tin của Book từ BookID
+                    .ToList();
+
+                var total = 0;
+                foreach (var cart in userCarts)
+                {
+                    foreach (var book in _db.Books)
+                    {
+                        if (book.BookID == cart.BookID)
+                        {
+                            total += cart.Total;
+                        }
+                    }
+
+                }
+                ViewBag.userCarts = userCarts;
+                ViewBag.TotalPrice = total;
+
+                return View();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
         //ko can dang nhap, hien thi book
         public IActionResult Index()
         {
