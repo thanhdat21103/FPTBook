f515d2eaca5f96e0f8fa2eb7a36c26be961847e4
minhhoang0xx <118556260+minhhoang0xx@users.noreply.github.com>
2023-10-24 01:56:12 +0700

    add admin controler
    add admin controler

---
+++ ASMNhom3/ASMNhom3/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASMNhom3.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+        private readonly ASMNhom3Context _db;
+        public AdminController(ILogger<AdminController> logger, ASMNhom3Context context)
+        {
+            _logger = logger;
+            _db = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
